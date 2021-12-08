e8480e7e30cc1d363ff0c960de279a3e35a82d27
Keith Price <keithprice.dev@gmailcom>
2021-12-08 15:39:24 +0700

    changes to layout, experimenting with different font sizes, added media queries for 1200px

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,18 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
 
-
 export default function Header() {
 	return (
 		<header>
-			<p>Astronomy Picture of the Day</p>
-			<ul>
-				<li>
-					<Link to="/">APOD</Link>
-					<Link to="/archive">Archive</Link>
-				</li>
-			</ul>
+			<div className="header-container">
+				<p>Astronomy Picture of the Day</p>
+				<ul>
+					<li>
+						<Link to="/">APOD</Link>
+						<Link to="/archive">Archive</Link>
+					</li>
+				</ul>
+			</div>
 		</header>
 	);
 }
